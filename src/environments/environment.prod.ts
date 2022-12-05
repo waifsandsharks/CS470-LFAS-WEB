865ca8e17bc7914007c238932e258a77cc4bf3ad
waifsandsharks <Waifsandsharks@Gmail.com>
2022-12-04 23:06:12 -0600

    updated Enviroment API link and Main URL handling

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://fmyxngtkuk.execute-api.us-east-1.amazonaws.com'
 };
